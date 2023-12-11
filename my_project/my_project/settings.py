cbfa56d36150191b1cfc18d24824b8e64bc2ba1b
anatoly <tolyan.yalta@yandex.ru>
2023-12-11 21:11:08 +0300

    The file settings.py has been changed. Adding the path to the directory for the basic project template.

--- my_project/my_project/settings.py
+++ my_project/my_project/settings.py
@@ -57,7 +57,9 @@ ROOT_URLCONF = 'my_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'templates',
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
