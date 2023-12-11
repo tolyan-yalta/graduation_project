2d7eab10c659729d591d4ce22e14dd70a589704f
anatoly <tolyan.yalta@yandex.ru>
2023-12-11 21:19:43 +0300

    File 'views.py 'to the application 'index_app' changed.

--- my_project/index_app/views.py
+++ my_project/index_app/views.py
@@ -6,9 +6,10 @@ from django.http import HttpResponse
 
 
 def index(request):
-    html = """
-    <h1>Главная страница.</h1>
-    <p>Это главная страница дипломного проекта.</p>
-    """
+    # html = """
+    # <h1>Главная страница.</h1>
+    # <p>Это главная страница дипломного проекта.</p>
+    # """
     # logger.info("The main page has been loaded.")
-    return HttpResponse(html)
+    return render(request, 'index_app/index.html')
+    # return HttpResponse(html)
