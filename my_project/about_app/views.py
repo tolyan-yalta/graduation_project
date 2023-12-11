4edc82c109f0c73b4214afed8a184f8356a18324
anatoly <tolyan.yalta@yandex.ru>
2023-12-11 21:28:52 +0300

    File 'views.py 'to the application 'about_app' changed.

--- my_project/about_app/views.py
+++ my_project/about_app/views.py
@@ -6,29 +6,29 @@ from django.http import HttpResponse
 
 
 def about(request):
-    html = """
-    <h1>Страница "О нас".</h1>
-    <p>Это страница проекта "О нас".</p>
-    """
+    # html = """
+    # <h1>Страница "О нас".</h1>
+    # <p>Это страница проекта "О нас".</p>
+    # """
     # logger.info("The about page has been uploaded.")
-    # return render(request, 'about_app/about.html')
-    return HttpResponse(html)
+    return render(request, 'about_app/about.html')
+    # return HttpResponse(html)
 
 
 def contacts(request):
-    html = """
-    <h1>Страница "Контакты".</h1>
-    <p>Это страница проекта "Контакты".</p>
-    """
+    # html = """
+    # <h1>Страница "Контакты".</h1>
+    # <p>Это страница проекта "Контакты".</p>
+    # """
     # logger.info("The contacts page has been uploaded.")
-    # return render(request, 'about_app/contacts.html')
-    return HttpResponse(html)
+    return render(request, 'about_app/contacts.html')
+    # return HttpResponse(html)
 
 def gallery(request):
-    html = """
-    <h1>Страница "Галерея".</h1>
-    <p>Это страница проекта "Галерея".</p>
-    """
+    # html = """
+    # <h1>Страница "Галерея".</h1>
+    # <p>Это страница проекта "Галерея".</p>
+    # """
     # logger.info("The gallery page has been uploaded.")
-    # return render(request, 'about_app/gallery.html')
-    return HttpResponse(html)
+    return render(request, 'about_app/gallery.html')
+    # return HttpResponse(html)
