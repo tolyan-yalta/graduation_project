a4d8b088041a03860c0c8f74334e5809d802ce35
anatoly <tolyan.yalta@yandex.ru>
2023-12-13 21:44:57 +0300

    The users_app application has been added to the graduation project

---
+++ my_project/users_app/urls.py
@@ -0,0 +1,21 @@
+from django.urls import path
+from . import views
+from django.contrib.auth.views import LogoutView
+from my_project import settings
+# from django.contrib.auth import views as auth_views
+ 
+app_name = "users"
+
+urlpatterns = [
+    # path('login/', views.login_user, name='login'),
+    path('login/', views.LoginUser.as_view(), name='login'),
+    path('logout/', views.logout_user, name='logout'),
+    path('register/', views.register, name='register'),
+    # path('logout/', LogoutView.as_view(next_page=settings.LOGOUT_REDIRECT_URL), name='logout'),
+    # path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
+    # path('logout/', LogoutView.as_view(), name='logout'),
+]
+
+# http://127.0.0.1:8000
+# http://127.0.0.1:8000/users/login
+# http://127.0.0.1:8000/users/logout
---
+++ my_project/users_app/views.py
@@ -0,0 +1,58 @@
+from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.forms import AuthenticationForm
+from django.urls import reverse, reverse_lazy
+
+from users_app.forms import LoginUserForm, RegisterUserForm
+
+
+class LoginUser(LoginView):
+    # form_class = AuthenticationForm   
+    form_class = LoginUserForm
+    template_name = 'users_app/login.html'
+    extra_context = {'title': "Авторизация"}
+
+    # def get_success_url(self):
+    #     # return reverse_lazy('home')
+    #     return reverse_lazy('catalog')
+    
+
+# def login_user(request):
+#     # return HttpResponse("login")
+#     # form = LoginUserForm()
+#     # return render(request, 'users_app/login.html', {'form': form})
+#     if request.method == 'POST':
+#         form = LoginUserForm(request.POST)
+#         if form.is_valid():
+#             cd = form.cleaned_data
+#             user = authenticate(request, username=cd['username'], 
+#                                 password=cd['password'])
+#             if user and user.is_active:
+#                 login(request, user)
+#                 # return HttpResponseRedirect(reverse('home'))
+#                 return HttpResponseRedirect(reverse('catalog'))
+                
+#     else:
+#         form = LoginUserForm()
+#     return render(request, 'users_app/login.html', {'form': form})
+
+
+def logout_user(request):
+    logout(request)
+    # return HttpResponseRedirect(reverse('users:login'))
+    return HttpResponseRedirect(reverse('about'))
+
+
+def register(request):
+    if request.method == "POST":
+        form = RegisterUserForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)  # создание объекта без сохранения в БД
+            user.set_password(form.cleaned_data['password'])
+            user.save()
+            return render(request, 'users_app/register_done.html')
+    else:
+        form = RegisterUserForm()
+    return render(request, 'users_app/register.html', {'form': form})
