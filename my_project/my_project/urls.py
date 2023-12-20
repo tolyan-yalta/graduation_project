f8e7d1dd15b097a3ca08a887b219a40571d2f35e
anatoly <tolyan.yalta@yandex.ru>
2023-12-20 21:20:06 +0300

    The file my_project/urls.py has been changed

--- my_project/my_project/urls.py
+++ my_project/my_project/urls.py
@@ -15,12 +15,21 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+# from django.conf.urls import url
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('index_app.urls')),
+    # path('basket/', include('basket_app.urls', namespace='basket')),
+    re_path(r'^basket/', include('basket_app.urls', namespace='basket')),
+    # path('catalog/', include('catalog_app.urls', namespace='catalog')),
     path('catalog/', include('catalog_app.urls')),
     path('about/', include('about_app.urls')),
     path('users/', include('users_app.urls', namespace="users")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
