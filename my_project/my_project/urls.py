b250a5be53eb0a9ad55d62dd624064a386914905
anatoly <tolyan.yalta@yandex.ru>
2023-12-20 23:43:35 +0300

    The file urls.py has been changed.

--- my_project/my_project/urls.py
+++ my_project/my_project/urls.py
@@ -1,19 +1,3 @@
-"""
-URL configuration for my_project project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include, re_path
 # from django.conf.urls import url
@@ -29,6 +13,9 @@ urlpatterns = [
     path('catalog/', include('catalog_app.urls')),
     path('about/', include('about_app.urls')),
     path('users/', include('users_app.urls', namespace="users")),
+    re_path(r'^orders/', include('orders_app.urls', namespace='orders')),
+    # re_path(r'^orders/', include('orders_app.urls')),
+    # path('orders/', include('orders_app.urls', namespace='orders')),
 ]
 
 if settings.DEBUG:
