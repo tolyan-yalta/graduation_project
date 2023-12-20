915b9aaecc4d6e3dab63ebba6c23d8969da9a0e2
anatoly <tolyan.yalta@yandex.ru>
2023-12-20 21:45:29 +0300

    A file has been created 'menu.py' in the 'index_app' application.

---
+++ my_project/index_app/menu.py
@@ -0,0 +1,6 @@
+menu = [{'title': "Главная страница", 'url_name': 'index'},
+        {'title': "Каталог товаров", 'url_name': 'catalog:catalog'},
+        {'title': "Галерея", 'url_name': 'gallery'},
+        {'title': "О нас", 'url_name': 'about'},
+        {'title': "Контакты", 'url_name': 'contacts'},
+        ]
