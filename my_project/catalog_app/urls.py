d361be918af5f75ca011d56cceaaf2aa8c0bf925
anatoly <tolyan.yalta@yandex.ru>
2023-12-20 21:47:16 +0300

    The file catalog_app/urls.py has been changed.

--- my_project/catalog_app/urls.py
+++ my_project/catalog_app/urls.py
@@ -1,8 +1,22 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
+
+from django.conf.urls.static import static
+from django.conf import settings
+
+app_name = "catalog"
 
 urlpatterns = [
     path('', views.catalog, name='catalog'),
+    # path('category/<int:cat_id>/', views.show_category, name='category'),
+    path('category/<slug:cat_slug>/', views.show_category, name='category'),
+    # path('product/<int:id>/<slug:slug>/', views.product_detail, name='product_detail'),
+    re_path(r'^(?P<id>\d+)/(?P<slug>[-\w]+)/$',
+        views.product_detail, name='product_detail'),
     ]
 
+# включаем возможность обработки картинок
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 # http://127.0.0.1:8000/catalog
+
