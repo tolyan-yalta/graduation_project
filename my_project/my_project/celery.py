8f6716ef8c96aa5da1f311c8ffbf3b614bb29a8e
anatoly <tolyan.yalta@yandex.ru>
2024-01-13 20:09:30 +0300

    The file 'my_project/celery.py' has been changed.

--- my_project/my_project/celery.py
+++ my_project/my_project/celery.py
@@ -3,7 +3,7 @@ import os
 from celery import Celery
 
 # Set the default Django settings module for the 'celery' program.
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'my_project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'my_project.work_settings')
 
 app = Celery('my_project')
 
@@ -17,7 +17,7 @@ app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
 
 
-@app.task(bind=True, ignore_result=True)
-def debug_task(self):
-    print(f'Request: {self.request!r}')
+# @app.task(bind=True, ignore_result=True)
+# def debug_task(self):
+#     print(f'Request: {self.request!r}')
     
